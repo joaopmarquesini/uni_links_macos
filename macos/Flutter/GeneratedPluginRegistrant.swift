dfe02c208ff4e92cf1fb229c8f7c9393bd2e4ca5
João Paulo Marquesini <jao@markezine.me>
2021-02-08 00:17:30 -0300

    initial commit

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import uni_links_fde
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  UniLinksPlugin.register(with: registry.registrar(forPlugin: "UniLinksPlugin"))
+}
